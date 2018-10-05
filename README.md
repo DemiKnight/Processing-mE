6ec81ead5e6d602f18ce0d36dddf7a28375833dc
Alex Knight <alexknight133@icloud.com>
2018-10-05 17:04:46 +0100

    Basic Processing Setup
    1) Added Processing to the dependencies list
    2) Basic file to startup the processing window

---
+++ README.md
@@ -0,0 +1,4 @@
+# Description
+--- 
+This is a to house a test game being developed with [Processing 3](https://processing.org/), 
+at this point the game has no real purpose.
---
+++ src/main/java/com/krypticalKnight/processingGame/ProcessingGame.java
@@ -0,0 +1,13 @@
+package com.krypticalKnight.processingGame;
+
+import processing.core.PApplet;
+
+public class ProcessingGame extends PApplet{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+        PApplet.main("com.krypticalKnight.processingGame.ProcessingGame");
+
+    }
+}
