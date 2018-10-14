642c18e17d611c1a0cdb202d2d5ff0407e795fb3
Kryptical <alexknight133@icloud.com>
2018-10-14 21:00:07 +0100

    Added comments to json reader + Hash Map
    1) Added comments to JsonReader
    2) Added Hashmap to JsonReader, which will store all the values

--- src/main/java/util/JsonReader.java
+++ src/main/java/util/JsonReader.java
@@ -6,14 +6,31 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class JsonReader {
 
+    /**
+     * Will store the file to be read.
+     * @implNote May be removed in the future.
+     */
     private File fileToRead;
+
+    /**
+     * Store the loaded config lines in here until it can be converted.
+     * @implNote May be removed in the future.
+     */
     private List<String> lines;
 
 
+    private HashMap<String, String> storecMap;
+
+    /**
+     * This will read the file specified and store it within the class, converted.
+     *
+     * @param fileName Name of the file to be read
+     */
     public JsonReader(String fileName)
     {
         lines = Collections.emptyList();
@@ -22,16 +39,21 @@ public class JsonReader {
         {
             URL url = JsonReader.class.getResource(fileName);
 
+            //Read the loaded file
             lines = Files.readAllLines(Paths.get(url.toURI()), StandardCharsets.UTF_8);
-
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
+
+
     }
 
-    private void ReadLines(){
+    /**
+     * Convert all the lines in the {@link JsonReader#lines} to json format
+     */
+    private void convertLines(){
 
     }
 
