528e259d4e26ca44dd34133e219d05ef68b70321
Kryptical <alexknight133@icloud.com>
2018-11-14 13:21:40 +0000

    Render states comments + WIP state
    1) Added comments to RENDER_STATES
    2) Added unimplemented 'hidden' state which will stop the entity from
    rendering but keep updating it

--- src/main/java/render/RENDER_STATES.java
+++ src/main/java/render/RENDER_STATES.java
@@ -1,6 +1,16 @@
 package render;
 
+/**
+ * The states that would effect how the entity would be rendered.
+ * <ul>
+ *     <li><tt>halted</tt> -The render should be stopped, next time update() is called </li>
+ *     <li><tt>rendering</tt> - Continue to render to the processing window.</li>
+ *     <li><tt>hidden</tt> - WIP continue to run "update" but remove from renderList</li>
+ * </ul>
+ * @see RenderManager
+ */
 public enum RENDER_STATES {
-    halted,
-    rendering
+    halted, //The render should be stopped, next time the update() is ran
+    rendering, //Continue to render to the processing window.
+    hidden // WIP continue to run "update" but remove from renderList
 }
