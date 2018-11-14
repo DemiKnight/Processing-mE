733fb3f85126a138a66e0e3d768ce99d218bcd8a
Kryptical <alexknight133@icloud.com>
2018-11-14 13:41:40 +0000

    WIP EntityManager class
    1) Added EntityManager to hold all entities being worked on

---
+++ src/main/java/EntityManager.java
@@ -0,0 +1,15 @@
+import render.Entity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class EntityManager {
+
+
+    List<Entity> entityList;
+
+    public EntityManager()
+    {
+        entityList = new LinkedList<>();
+    }
+}
