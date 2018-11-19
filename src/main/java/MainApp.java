2537e3489c1438c461f2503aceddb179ebbf7317
Kryptical <alexknight133@icloud.com>
2018-11-19 19:58:32 +0000

    Version increment + WIP Resource Manager
    1) Incremented the version to v0.0.4
    2) Added methods and storage lists to the resource manager. Also added
    documentation to everything within the class. Need to add implementation
    in the Entity class

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,28 +4,29 @@ import render.ResourceManager;
 
 public class MainApp extends PApplet{
 
-    private static String version = "0.0.3";
+    private static String version = "0.0.4";
 
 //    private ConfigManager configM = new ConfigManager();
 
     private static RenderManager renderM;
     private static EntityManager entityM;
     private static ResourceManager resourceM;
+    private static PApplet instance;
 
 
     public static void main(String[] args)
     {
-        resourceM = new ResourceManager();
+        resourceM = new ResourceManager(instance);
 
         renderM = new RenderManager(resourceM);
         entityM = new EntityManager(renderM,resourceM);
-        
         PApplet.main("MainApp");
     }
 
     public void settings()
     {
         size(250,250);
+        instance = this;
     }
 
     public void setup()
--- src/main/java/render/ResourceManager.java
+++ src/main/java/render/ResourceManager.java
@@ -1,5 +1,147 @@
 package render;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PShape;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The object which will hold all visual data, preventing data redundancy and exceeding load times.
+ * Currently supporting PImage & PShape.
+ * @author Alex James Knight
+ * @since 0.0.4
+ * @see Entity
+ * @see RenderManager
+ */
 public class ResourceManager {
 
+    /**
+     * The types currently supported, relating to the lists
+     */
+    enum supportedTypes{
+        PShape,
+        PImage
+    }
+
+    /**
+     * The List storing all the image data. Using the processing class PImage.
+     */
+    private List<PImage> imageList;
+
+    /**
+     * The list storing all the shape data. Using the processing class PShape
+     */
+    private List<PShape> shapeList;
+
+    /**
+     * LInk to the parent processing instance.
+     */
+    private PApplet parent;
+
+    /**
+     *
+     * @param PAppletP A link to the parent processing instance.
+     */
+    public ResourceManager(PApplet PAppletP)
+    {
+        this.parent = PAppletP;
+
+        this.shapeList = new LinkedList<>();
+        this.imageList = new LinkedList<>();
+    }
+
+    public PImage getPImageResource(int index)
+    {
+        return imageList.get(index);
+    }
+
+    public PShape getPShapeResource(int index)
+    {
+        return shapeList.get(index);
+    }
+//    public <T> getResource(int index, supportedTypes type)
+//    {
+//        T obj = new T();
+//
+//
+////        return imageList.get(index);
+//    }
+
+    /**
+     * Will add the given image, using <tt>loadImage()</tt>, to the list and make it available for use.
+     * @param resource The PImage to be stored.
+     */
+    public void addResource(PImage resource)
+    {
+        this.imageList.add(resource);
+    }
+
+    /**
+     * WIll add the shape, using <tt>loadShape()</tt>, to the list and make it available for use.
+     * @param resource The PShape to be stored.
+     */
+    public void addResource(PShape resource)
+    {
+        this.shapeList.add(resource);
+    }
+
+    /**
+     * Will add the given image, using <tt>loadImage()</tt>, to the list and make it available for use.
+     * @param resource The PImage to be stored.
+     * @return The index of the resource just stored.
+     */
+    public int addResourceI(PImage resource)
+    {
+        this.imageList.add(resource);
+        return this.imageList.size() - 1;
+    }
+
+    /**
+     * WIll add the shape, using <tt>loadShape()</tt>, to the list and make it available for use.
+     * @param resource The PShape to be stored
+     * @return THe index of the resource just stored.
+     */
+    public int addResourceI(PShape resource)
+    {
+        this.shapeList.add(resource);
+        return this.imageList.size() - 1;
+    }
+
+    /**
+     * Remove a resource from the Resource Manager, specifically from the image Store
+     * @param resourceToRemove The resource to remove from the imageList
+     */
+    public void removeResource(PImage resourceToRemove)
+    {
+        imageList.remove(resourceToRemove);
+    }
+
+    /**
+     * Remove a resource from the Resource Manager, specifically from the shape Store
+     * @param resourceToRemove The resource to remove from the shapeList
+     */
+    public void removeResource(PShape resourceToRemove)
+    {
+        shapeList.remove(resourceToRemove);
+    }
+
+    /**
+     * Remove a resource from the Resource Manager, using the index and a given type.
+     * @param index The index of the resource to remove.
+     * @param type The type of resource, indicating wether it is a PShape or PImage.
+     */
+    public void removeResource(int index, supportedTypes type)
+    {
+        switch (type)
+        {
+            case PImage:
+                imageList.remove(index);
+                break;
+            case PShape:
+                shapeList.remove(index);
+                 break;
+        }
+    }
 }
